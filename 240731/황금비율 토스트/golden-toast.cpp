d5de59bb7d626dc64e1e69ac2997015aac9c49b5
JINK004 <98154561+JINK004@users.noreply.github.com>
2024-07-31 22:48:33 +0900

    Title: 황금비율 토스트, Time: 29ms, Memory: 3MB, Status: Passed - CodeTree

--- 240731/황금비율 토스트/golden-toast.cpp
+++ 240731/황금비율 토스트/golden-toast.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 int main() {
     list<char> mlist;
-    list<char>::iterator it; 
-    it = mlist.end();
 
     int n, m;
     cin >> n >> m;
@@ -14,32 +12,36 @@ int main() {
         cin >> tmp;
         mlist.push_back(tmp);
     }
+
+    list<char>::iterator it; 
+    it = mlist.end();
+
     for (int i=0;i<m;i++){
         char tmp;
         cin >> tmp;
         if(tmp == 'L'){
             if(it == mlist.begin()) continue;
-            it++;
+            it--;
         }
         else if(tmp == 'R'){
             if(it == mlist.end()) continue;
-            it--;
+            it++;
         }
         else if(tmp == 'D'){
             if(it == mlist.end()) continue;
-            mlist.erase(it);
+            it = mlist.erase(it);
         }
         else if(tmp == 'P'){
             char num;
             cin >> num;
-            mlist.insert(num);
+            mlist.insert(it, num);
         }
         else{
             cout << "ERROR\n";
         }
     }
 
-    for (it=mlist.begin();it<mlist.end();it++){
+    for (it=mlist.begin(); it!=mlist.end(); it++){
         cout << *it;
     }
     return 0;

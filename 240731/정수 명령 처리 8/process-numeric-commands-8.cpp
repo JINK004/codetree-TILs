e0f0db752cadc379945f4c72bce8bc2ee8280a40
JINK004 <98154561+JINK004@users.noreply.github.com>
2024-07-31 14:09:03 +0900

    Title: 정수 명령 처리 8, Time: 22ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    list<int> mlist;
+    int n;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        string tmp;
+        cin >> tmp;
+        if (tmp == "push_back"){
+            int num;
+            cin >> num;
+            mlist.push_back(num);
+        }
+        else if(tmp == "push_front"){
+            int num;
+            cin >> num;
+            mlist.push_front(num);
+        }
+        else if(tmp == "pop_front"){
+            int num = mlist.front();
+            mlist.pop_front();
+            cout << num << "\n";
+        }
+        else if(tmp == "pop_back"){
+            int num = mlist.back();
+            mlist.pop_back();
+            cout << num << "\n";
+        }
+        else if(tmp == "size"){
+            cout << mlist.size() << "\n";
+        }
+        else if(tmp == "empty"){
+            if (mlist.empty()) cout << "0\1\n";
+            else cout << "0\n";
+        }
+        else if(tmp == "front"){
+            cout << mlist.front() << "\n";
+        }
+        else if(tmp == "back"){
+            cout << mlist.back() << "\n";
+        }
+        else {
+            cout << "Error\n";
+        }
+    }
+    return 0;
+}
